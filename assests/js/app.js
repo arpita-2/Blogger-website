d39e50eba875c882a3b788ce3f99bdc326f788b2
arpita-2 <dasguptaarpita1999@gmail.com>
2022-07-22 00:42:28 +0530

    blog

--- assests/js/app.js
+++ assests/js/app.js
@@ -9,4 +9,22 @@ $(document).ready(function () {
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
+
+/**$ is a jquery tag to select object and it is easy to use */
+/**owl carosuel file is linked because without linking it will not show in the live port , I have to call the owl carousel fumction*/
+/**owl carousel for blog */
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: false,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+   
+});
+/**autoplaytimeout has given 3 sec because to rotate it every 3 sec automatically */
+/**dots has been given to false because to remove the dots from the carousel */
+/**nav: true  is a scroll<> navigation design of owl carousel */
+/**calling jquery query tag with owl-carousel object to create a arrow on the website */
+
 })
